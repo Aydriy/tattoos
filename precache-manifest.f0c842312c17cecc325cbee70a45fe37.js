4c5f795a00d3eb5666ebf804070b267ddd311aa2
Aydriy <smitas4511@gmail.com>
2020-05-10 23:50:32 +0300

    Updates

---
+++ precache-manifest.f0c842312c17cecc325cbee70a45fe37.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ee6ea341e51239b8d18f910295730b",
+    "url": "/tattoos/index.html"
+  },
+  {
+    "revision": "c0c15ba9b34b9626fa18",
+    "url": "/tattoos/static/css/main.549fab29.chunk.css"
+  },
+  {
+    "revision": "e2c82d2c97bfa93f9476",
+    "url": "/tattoos/static/js/2.ffd20765.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/tattoos/static/js/2.ffd20765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c15ba9b34b9626fa18",
+    "url": "/tattoos/static/js/main.2bfb28d4.chunk.js"
+  },
+  {
+    "revision": "2de0b8558abe9a70cc17",
+    "url": "/tattoos/static/js/runtime-main.48b002c3.js"
+  },
+  {
+    "revision": "67a3fa4a1581165b856513924bb5514b",
+    "url": "/tattoos/static/media/MaskGroup.67a3fa4a.jpg"
+  },
+  {
+    "revision": "ccb326a6cef8dc77715c49f244fca878",
+    "url": "/tattoos/static/media/PlayfairDisplaySC-Black.ccb326a6.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/tattoos/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "80f50ed45634e4701ca752417366b324",
+    "url": "/tattoos/static/media/aboutUsBg.80f50ed4.png"
+  },
+  {
+    "revision": "948857d34c303a23e7e9b89276ee4559",
+    "url": "/tattoos/static/media/aboutUsImage.948857d3.png"
+  },
+  {
+    "revision": "256f5834476f18c9b078abb1394f39c6",
+    "url": "/tattoos/static/media/achivementBG.256f5834.png"
+  },
+  {
+    "revision": "4f52147438ee0a558bd3480b600bf0ae",
+    "url": "/tattoos/static/media/arrov.4f521474.svg"
+  },
+  {
+    "revision": "d6ba51c9f27c47c2526ae0ba5942939a",
+    "url": "/tattoos/static/media/image.d6ba51c9.svg"
+  },
+  {
+    "revision": "c3f8174b43c8ffc9607daee38413cfa7",
+    "url": "/tattoos/static/media/image1.c3f8174b.png"
+  },
+  {
+    "revision": "c1cf1ca7e8e5de231ec56c0ac04ab214",
+    "url": "/tattoos/static/media/image2.c1cf1ca7.png"
+  },
+  {
+    "revision": "58ebd576bd2be1c889c66aef789cd206",
+    "url": "/tattoos/static/media/image3.58ebd576.png"
+  },
+  {
+    "revision": "12a6f1e2100ca869677db81dc6d98f5b",
+    "url": "/tattoos/static/media/image4.12a6f1e2.png"
+  },
+  {
+    "revision": "8bda6338a4ca311f45b1433eb564c262",
+    "url": "/tattoos/static/media/person1.8bda6338.png"
+  },
+  {
+    "revision": "aa1228fcb6b909fe3e9b420b7d96a932",
+    "url": "/tattoos/static/media/person2.aa1228fc.png"
+  },
+  {
+    "revision": "ee750048684c3a6081ae0c986609f738",
+    "url": "/tattoos/static/media/person3.ee750048.png"
+  },
+  {
+    "revision": "0a79411682545aa4296c01a59d40d1de",
+    "url": "/tattoos/static/media/person4.0a794116.png"
+  },
+  {
+    "revision": "9c3da35892187f5b8f1a2f7ea43df948",
+    "url": "/tattoos/static/media/picture1.9c3da358.svg"
+  },
+  {
+    "revision": "32c3407aceb873523579293353e33240",
+    "url": "/tattoos/static/media/picture10.32c3407a.svg"
+  },
+  {
+    "revision": "6adc2924ed368ca463e59c638833996c",
+    "url": "/tattoos/static/media/picture11.6adc2924.svg"
+  },
+  {
+    "revision": "3038a19bcc85a619f0362f5371c906b9",
+    "url": "/tattoos/static/media/picture12.3038a19b.svg"
+  },
+  {
+    "revision": "0c9898c3d0b996e16cad1b063e5cc666",
+    "url": "/tattoos/static/media/picture13.0c9898c3.svg"
+  },
+  {
+    "revision": "41ac5cbcf90a5275412cd83f384c941b",
+    "url": "/tattoos/static/media/picture14.41ac5cbc.svg"
+  },
+  {
+    "revision": "aa78734ed513ecf9e45574b432ee3547",
+    "url": "/tattoos/static/media/picture2.aa78734e.svg"
+  },
+  {
+    "revision": "364c43f44845c46102ff1651e38256e3",
+    "url": "/tattoos/static/media/picture3.364c43f4.svg"
+  },
+  {
+    "revision": "6d81b025a1699e12bd169107d6ed3fc9",
+    "url": "/tattoos/static/media/picture4.6d81b025.svg"
+  },
+  {
+    "revision": "e7e46ccc8db865cc04abf3702fd4869b",
+    "url": "/tattoos/static/media/picture5.e7e46ccc.svg"
+  },
+  {
+    "revision": "39f273551c9af017433e363f8c418a94",
+    "url": "/tattoos/static/media/picture6.39f27355.svg"
+  },
+  {
+    "revision": "60ab056e943fdb0f83d85391b98259a0",
+    "url": "/tattoos/static/media/picture7.60ab056e.svg"
+  },
+  {
+    "revision": "68dd3989c1463349a6078c5190917451",
+    "url": "/tattoos/static/media/picture8.68dd3989.svg"
+  },
+  {
+    "revision": "d4373dcdcf448a7e355996d18c5d6bc2",
+    "url": "/tattoos/static/media/picture9.d4373dcd.svg"
+  }
+]);
